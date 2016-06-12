9c31378951f79478a32aa3240471390fa34d2cbd
Kristopher Micinski <micinski@umiacs.umd.edu>
2016-06-11 22:07:22 -0400

    adding travis files

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,7 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind ounit re"
+OPAM_DEPENDS="sha str unix ppx_deriving.std ppx_deriving_yojson yojson"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
